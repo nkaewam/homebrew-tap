20afcb095e2124450958002e9f69d26fcf026796
nonkaewampor <nonthapat.kaewamporn1@agoda.com>
2026-07-24 03:23:19 +0700

    fix: install git-clonepath from release binaries

--- Formula/git-clonepath.rb
+++ Formula/git-clonepath.rb
@@ -1,16 +1,34 @@
 class GitClonepath < Formula
   desc "Clone hosted Git repositories into deterministic paths"
   homepage "https://github.com/nkaewam/git-clonepath"
-  url "https://github.com/nkaewam/git-clonepath/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "3abcde28c861646e286aa5247f760ce2424b4476ee5d2f522aec8e3e7f15edcc"
-  head "https://github.com/nkaewam/git-clonepath.git", branch: "main"
+  version "0.1.1"
 
-  depends_on "go" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/nkaewam/git-clonepath/releases/download/v0.1.1/git-clonepath_0.1.1_darwin_arm64.tar.gz"
+      sha256 "1d20119208473fd36f21fb1df8b82bb80097189c9e1666c7e056a93066e2ef84"
+    end
+
+    on_intel do
+      url "https://github.com/nkaewam/git-clonepath/releases/download/v0.1.1/git-clonepath_0.1.1_darwin_amd64.tar.gz"
+      sha256 "83a93803a6b6c5a4731b481dc41f4f5da01a6cd044e1df35b02d492f3725b303"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/nkaewam/git-clonepath/releases/download/v0.1.1/git-clonepath_0.1.1_linux_arm64.tar.gz"
+      sha256 "65186c79e7f892ea9391fed7c96c8a44dc5c3306b1299734617239dbee29b780"
+    end
+
+    on_intel do
+      url "https://github.com/nkaewam/git-clonepath/releases/download/v0.1.1/git-clonepath_0.1.1_linux_amd64.tar.gz"
+      sha256 "75e51e3f20f2c174db073388473871daccde177bb60dc5d8fe5b764a67043529"
+    end
+  end
 
   def install
-    system "go", "build",
-           *std_go_args(output: bin/"git-clonepath", ldflags: "-s -w"),
-           "./cmd/git-clonepath"
+    bin.install "git-clonepath"
   end
 
   test do
